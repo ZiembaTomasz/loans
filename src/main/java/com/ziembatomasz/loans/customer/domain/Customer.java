2a7583a59a422f04342a5afd1721dc8a91adb84d
Tomasz Ziemba <ziembatomasz@gmail.com>
2020-02-15 21:27:16 +0100

    initial customer project

---
+++ src/main/java/com/ziembatomasz/loans/customer/domain/Customer.java
@@ -0,0 +1,12 @@
+package com.ziembatomasz.loans.customer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Customer {
+}
---
+++ src/main/java/com/ziembatomasz/loans/customer/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.ziembatomasz.loans.customer.repository;
+
+
+import com.ziembatomasz.loans.customer.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
+
